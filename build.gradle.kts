e42d9bbb652d999076dbf55c7cf4ec8b2b345345
João Daniel <jotaf.daniel@gmail.com>
2024-04-03 09:54:02 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+val javalin_version: String by project
+val slf4j_version: String by project
+
+plugins {
+    id("java")
+}
+
+group = "it.unibz.butterfly_net"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:$javalin_version")
+    implementation("org.slf4j:slf4j-simple:$slf4j_version")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/it/unibz/butterfly_net/ingestion_api/Component.java
@@ -0,0 +1,7 @@
+package it.unibz.butterfly_net.ingestion_api;
+
+public class Component {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
