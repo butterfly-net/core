b50cbc1dabdf13ad849949a34206ff3692f02c30
João Daniel <jotaf.daniel@gmail.com>
2024-04-03 12:27:43 +0200

    expose ingestion in Http server

--- src/main/java/it/unibz/butterfly_net/ingestion_api/Component.java
+++ src/main/java/it/unibz/butterfly_net/ingestion_api/Component.java
@@ -1,7 +1,13 @@
 package it.unibz.butterfly_net.ingestion_api;
 
+import it.unibz.butterfly_net.ingestion_api.application.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Component {
+    private static final Logger logger = LoggerFactory.getLogger(Component.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        HttpServer.run(logger);
     }
 }
---
+++ src/main/java/it/unibz/butterfly_net/ingestion_api/application/HttpServer.java
@@ -0,0 +1,64 @@
+package it.unibz.butterfly_net.ingestion_api.application;
+
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+import it.unibz.butterfly_net.ingestion_api.core.IngestionService;
+import it.unibz.butterfly_net.ingestion_api.core.ProjectRepository;
+import it.unibz.butterfly_net.ingestion_api.core.RawDataRepository;
+import it.unibz.butterfly_net.ingestion_api.core.errors.CredentialsMismatchError;
+import it.unibz.butterfly_net.ingestion_api.core.errors.MissingRequiredHeaderError;
+import it.unibz.butterfly_net.ingestion_api.core.errors.ProjectNotFoundError;
+import org.slf4j.Logger;
+
+import java.util.Map;
+
+public class HttpServer {
+    private static final Integer PORT = 8888;
+
+    public static void run(Logger logger) {
+        Javalin app = Javalin.create();
+
+        app.before(ctx -> {
+            logger.info(String.format("%s %s", ctx.method(), ctx.path()));
+            logger.info(ctx.headerMap().toString());
+            logger.info(ctx.queryParamMap().toString());
+            logger.info(ctx.body());
+        });
+
+        ProjectRepository projectRepository = new InMemoryProjectRepository();
+        RawDataRepository rawDataRepository = new InMemoryRawDataRepository();
+
+        app.post("/ingest", ctx -> new IngestionService(rawDataRepository, projectRepository)
+                .ingest(ctx.headerMap(), ctx.queryParamMap(), ctx.body())
+        );
+
+        app.exception(ProjectNotFoundError.class, (pnfe, ctx) -> {
+            String errorMessage = pnfe.getMessage();
+
+            ctx.status(HttpStatus.BAD_REQUEST);
+            ctx.json(Map.of(
+                    "error", errorMessage
+            ));
+        });
+
+        app.exception(MissingRequiredHeaderError.class, (mrhe, ctx) -> {
+            String errorMessage = mrhe.getMessage();
+
+            ctx.status(HttpStatus.BAD_REQUEST);
+            ctx.json(Map.of(
+                    "error", errorMessage
+            ));
+        });
+
+        app.exception(CredentialsMismatchError.class, (cme, ctx) -> {
+            String errorMessage = cme.getMessage();
+
+            ctx.status(HttpStatus.UNAUTHORIZED);
+            ctx.json(Map.of(
+                    "error", errorMessage
+            ));
+        });
+
+        app.start(PORT);
+    }
+}
